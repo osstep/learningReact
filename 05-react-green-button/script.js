918ad47c5db03067084021b143026b17d440f14b
Stepan Osadsi <osstep3@gmail.com>
2023-05-09 14:20:48 +0300

    commit

---
+++ 05-react-green-button/script.js
@@ -0,0 +1,20 @@
+
+const App = (props) => {
+  const { initialButtonText, initialButtonClass } = props
+  const [buttonText, setButtonText] = React.useState(initialButtonText)
+  const [buttonClass, setButtonClass] = React.useState(initialButtonClass)
+  const onButtonClick = () => {
+  setButtonText('Hello from React')
+  setButtonClass('green-btn')
+}
+  return (
+    <div className="app">
+    <button className={buttonClass} onClick={onButtonClick}>
+      {buttonText}
+    </button>
+  </div>
+  )
+}
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot(container)
+root.render(<App initialButtonText="Click me!" initialButtonClass=""></App>)
